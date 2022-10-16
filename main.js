bc02c2b13c3015f0dbfbdc8d4154b3835a483fa4
sido200 <93400204+sido200@users.noreply.github.com>
2022-10-16 13:51:03 +0100

    v

--- main.js
+++ main.js
@@ -3,8 +3,31 @@ let shop=document.querySelector('.shop-cart');
 let closebtn=document.querySelector('.close');
 let favbtn=document.querySelectorAll('.fav');
 let addbtn=document.querySelectorAll('.add');
+var grid=document.querySelectorAll('.grid');
 let cartProduct=document.querySelector('.cart-product')
 let totalprice=document.querySelector('.totalprice')
+let buy=document.querySelector('.buy')
+
+for (var k = 0 ; k < favbtn.length; k++){
+grid[k].addEventListener('mouseover',function(){
+    let img=this.querySelectorAll('img')
+    img[0].style.display='none'
+    img[1].style.display='block'
+    
+},true)
+grid[k].addEventListener('mouseout',function(){
+    let img=this.querySelectorAll('img')
+    img[0].style.display='block'
+    img[1].style.display='none'
+    
+},true)}
+
+
+    
+
+
+
+
 //panier btn
 shopbtn.addEventListener('click',function(){
     shop.style.transform='translateX(0)';
@@ -37,9 +60,8 @@ for (var i = 0 ; i < favbtn.length; i++) {
     
     box.classList.add('box');
 
-    box.innerHTML=' <img src='+img +' alt="" class="image"><div class="detail"><h3 class="names">'+name +'</h3><h5 class="price">'+ price +'</h5><div class="input"> <button class="down"><i class="fa-solid fa-minus"></i></button><input type="number" name="value" value="1" min="1"id="quantity"><button class="up"><i class="fa-solid fa-plus"></i></button></div> </div><i class="fa-solid fa-trash dlt"></i>'
-    //sup btn
- 
+    box.innerHTML =' <img src='+img +' alt="" class="image"><div class="detail"><h3 class="names">'+name +'</h3><h5 class="price">'+ price +'</h5><div class="input"> <button class="down"><i class="fa-solid fa-minus"></i></button><input type="number" name="value" value="1" min="1"id="quantity"><button class="up"><i class="fa-solid fa-plus"></i></button></div> </div><i class="fa-solid fa-trash dlt"></i>'
+
     if(document.querySelector('.names')!= null){
     let cartelement =document.querySelectorAll('.names')
     
@@ -51,30 +73,90 @@ for (var i = 0 ; i < favbtn.length; i++) {
     }
     
     }if(a==0) {
-        cartProduct.appendChild(box)}}
+        cartProduct.appendChild(box)
+        var rprice=Number(price.slice(0, -1));
+       
+        var rtotal=Number( totalprice.innerText.slice(0, -1))
+        
+         rtotal=rtotal+rprice 
+         
+         totalprice.innerHTML=rtotal+'$';}
+        }
     else{
-        cartProduct.appendChild(box)}
+        cartProduct.appendChild(box)
+        let rprice=Number(price.slice(0, -1));
+        var rtotal=Number( totalprice.innerText.slice(0, -1))
+         rtotal=rtotal+rprice  
+         totalprice.innerHTML=rtotal+'$';}
+          
+         
+        
+//sup btn
     let dltbtn=document.getElementsByClassName('dlt');
     for (var j = 0 ; j < (dltbtn.length); j++){
         dltbtn[j].addEventListener('click',function(){
+         let rprice =this.parentElement.querySelector('.price').innerText.slice(0, -1);
+       let qnt=this.parentElement.querySelector('input').getAttribute('value');
+            rtotal=rtotal-rprice 
+            totalprice.innerHTML=(rtotal*qnt)+'$';
             this.parentElement.remove()
          })
     }
-    let input=document.querySelectorAll("input")
-    let plusbtn=document.querySelectorAll(".up")
-    let moinsbtn=document.querySelectorAll(".down")
-    for (var m = 0 ; m < (input.length); m++){
-  plusbtn[m].addEventListener('click',function(){
-input[m].value=Number(input[m].value)+1;
-  })
-  moinsbtn[m].addEventListener('click',function(){
-  input[m].value= Number(input[m].value)-1;
+    
+    //quantity button
+    const plusbtn = document.getElementsByClassName("up");
+         const moinsbtn = document.getElementsByClassName("down");
+         for (let m = 0; m < plusbtn.length; m++) {
+
+           plusbtn[m].addEventListener("click", function (event) {
+        
+
+          console.log(this.parentElement)
+            
+            let input =this.parentElement.querySelector("#quantity");
+             input.setAttribute(
+               "value",
+               Number(input.getAttribute("value")) + 1 //should have used getAttribute and setAttribute
+             );
+             totalprice.innerHTML=(rtotal*Number(input.getAttribute("value")))+'$'
+             
+           });
+           moinsbtn[m].addEventListener("click", function () {
+          
+            let input =this.parentElement.querySelector("#quantity");
+             if(Number(input.getAttribute("value"))>1){
+             input.setAttribute(
+               "value",
+               Number(input.getAttribute("value")) - 1 //should have used getAttribute and setAttribute
+               
+             );
+             totalprice.innerHTML=(rtotal*Number(input.getAttribute("value")))+'$'
+             ;}
+             
+             
+           });
+           
+         }
   
-    })
-
-    }
-
-
    ;})}
+      //buy
+      buy.addEventListener('click',function(){
+     elm=this.parentElement.querySelectorAll('.box');
+     console.log(elm);
+       if(elm.length!=0){
+        alert("successful purchase")
+        for (var s = 0 ; s < (elm.length); s++){
+            elm[s].remove();
+            let ttl=document.querySelector('.totalprice')
+            ttl.innerHTML=0+'$'
+            
+           }
+           
+       }
+       else{alert("add product")}
+      })
+   
+
+
   
   
